accbcf6d3d9c01d5ab14659d3eaf2e53ebefdf2a
Kamil Burek <kb@kamilb.info>
2025-03-12 21:13:09 +0000

    Upgrade to Astro 5.4 and Tailwind 4
    Update base packages
    
    Install new tailwind
    
    Changes from `npx @tailwindcss/upgrade`
    
    result from upgrade tool
    
    Fix sorting

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,12 @@
 import alpinejs from '@astrojs/alpinejs';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 import { BASE, DOMAIN, SHIKI_THEMES } from './src/shared/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx()],
+  integrations: [alpinejs(), mdx()],
   output: 'static',
   site: DOMAIN,
   base: BASE,
@@ -18,5 +17,8 @@ export default defineConfig({
     shikiConfig: {
       themes: SHIKI_THEMES
     }
+  },
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
